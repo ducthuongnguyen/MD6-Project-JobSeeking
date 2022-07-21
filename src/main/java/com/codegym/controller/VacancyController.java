c76e96830def8929e05e481086b5804f844026a0
qanh1996 <phamthulien1975@gmail.com>
2022-07-21 18:07:13 +0700

    update

--- src/main/java/com/codegym/controller/VacancyController.java
+++ src/main/java/com/codegym/controller/VacancyController.java
@@ -1,6 +1,11 @@
 package com.codegym.controller;
 
+import com.codegym.model.entity.Vacancy;
+import com.codegym.service.vacancy.IVacancyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vacancies")
 @CrossOrigin("*")
 public class VacancyController {
+    @Autowired
+    private IVacancyService vacancyService;
+
+    
 }
