0bcecc1eaf3f3d655d1ea378eeb049fc72b2e97d
huong2909 <99933730+huong2909@users.noreply.github.com>
2022-07-23 13:16:21 +0700

    update comsumes

--- src/main/java/com/codegym/controller/CompanyController.java
+++ src/main/java/com/codegym/controller/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,8 +38,8 @@ public class CompanyController {
         return new ResponseEntity<>(company.get(), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Company> update(@PathVariable Long id, @RequestBody Company company, MultipartFile file) {
+    @PutMapping(value = "/{id}",  consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Company> update(@PathVariable Long id, @RequestPart(value = "company") Company company,@RequestPart(value = "image", required = false) MultipartFile file) {
         Optional<Company> companyOptional = companyService.findById(id);
         if (!companyOptional.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
