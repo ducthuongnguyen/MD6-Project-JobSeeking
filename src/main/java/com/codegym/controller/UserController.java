a62cc00e56b8bc018b10d74468c90fdc3d4411d8
huong2909 <99933730+huong2909@users.noreply.github.com>
2022-07-26 14:58:49 +0700

    user giao diện ok

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.codegym.controller;
 
+import com.codegym.model.entity.Company;
 import com.codegym.model.entity.User;
 import com.codegym.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,23 @@ public class UserController {
         }
         return new ResponseEntity<>(user.get(), HttpStatus.OK);
     }
+    @GetMapping()
+    public ResponseEntity<Iterable<User>> findAll() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> user1 = userService.findById(id);
+        if (!user1.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        user.setId(user1.get().getId());
+        user.setUsername(user.getEmail());
+        user.setRoles(user1.get().getRoles());
+        user.setPassword(user1.get().getPassword());
+        userService.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
